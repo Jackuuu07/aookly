eb59efcaeeb168485b5599670cf1dd7578348052
kartik chaudhary <kartikchaudharih18@gmail.com>
2025-10-29 17:50:55 +0530

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    outDir: 'dist', // default for Vite, safe to keep
+  },
+  server: {
+    port: 5173, // used only for local development
+  },
+  base: './', // 👈 IMPORTANT for Render or GitHub Pages
 })
